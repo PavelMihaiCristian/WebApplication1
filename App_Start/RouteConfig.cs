88e5528de3fc76cb440997554dad6fb8f0d80228
PavelMihaiCristian <32361435+PavelMihaiCristian@users.noreply.github.com>
2017-11-15 13:16:09 +0100

    add controllers
    adding basic routes controllers

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebApplication1.Controllers;
 
 namespace WebApplication1
 {
@@ -11,13 +12,13 @@ namespace WebApplication1
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new { Controller = "Posts", Action = "Index" }, namespaces);
+            routes.MapRoute("Login", "login", new { Controller = "Auth", Action = "Login" }, namespaces);
+
         }
     }
 }
