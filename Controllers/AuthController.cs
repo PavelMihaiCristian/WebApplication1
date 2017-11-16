9b571aa41708856b489c520a05e82c42f088dbb3
PavelMihaiCristian <32361435+PavelMihaiCristian@users.noreply.github.com>
2017-11-16 09:22:19 +0100

    added basic login form
    added basic login form

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +12,25 @@ namespace WebApplication1.Controllers
         // GET: Auth
         public ActionResult Login()
         {
-            return View();
+            return View(new AuthLogin
+            {
+             
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+            if (form.UserName!="Rainbow dash")
+            {
+                ModelState.AddModelError("Username", "Username or password not valid");
+                return View(form);
+            }
+            return Content("The form is valid");
         }
     }
 }
---
+++ ViewModels/Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModels
+{
+   public class AuthLogin
+    {
+        [Required]
+        public String UserName { get; set; }
+
+
+        [Required, DataType(DataType.Password)]
+        public String Password { get; set; }
+    }
+}
